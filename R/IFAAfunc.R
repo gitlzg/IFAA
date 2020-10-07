4f1a7a5385a9085c736fe0380ecea5c6905259e3
unknown <lzg2151@gmail.com>
2020-10-07 12:04:48 -0400

    Add residual-permutation

--- R/IFAAfunc.R
+++ R/IFAAfunc.R
@@ -13,11 +13,12 @@
 ##' @param ctrlMany This takes logical value `TRUE` or `FALSE`. If `TRUE`, all variables except `testCov` are considered as control covariates provided `ctrlCov` is set to be `NULL`. The default value is `TRUE` which does not do anything if `ctrlCov` is not `NULL`.
 ##' @param nRef The number of randomly picked reference taxa used in phase 1. Default number is `40`.
 ##' @param nPermu The number of permutation used in phase 1. Default number is `40`.
+##' @param x1permut This takes a logical value `TRUE` or `FALSE`. If true, it will permute the variables in testCov. If false, it will use residual-permutation proposed by Freedman and Lane (1983). 
 ##' @param refTaxa Reference taxa will be used in phase 1. The default is `NULL` since the algorithm will pick those randomly. If a vector of taxa names are provided, the algorithm will use the provided taxa instead of randomly picking random reference taxa.
 ##' @param reguMethod regularization approach used in phase 1 of the algorithm. Take value `"mcp"` or `"lasso"`, default is `"mcp"`.
 ##' @param fwerRate The family wise error rate for identifying taxa/OTU/ASV associated with `testCov` in phase 1. Default is `0.25`.
 ##' @param paraJobs Number of parallel jobs that will be registered to run the algorithm. Default is `8`. If specified as `NULL`, it will automatically detect the cores to decide the number of parallel jobs.
-##' @param sequentialRun This takes a logical value `TRUE` or `FALSE`. Sometimes parallel jobs can not be successfully run for unknown reasons (such as hardware issues). For example, socket related errors may pop up or some slave cores return error message instead of numerical results. In those scenarios, setting `sequentialRun = TRUE` may help, but it will take more time to run. Default is `FALSE`.
+##' @param sequentialRun This takes a logical value `TRUE` or `FALSE`. Sometimes parallel jobs can not be successfully run for unknown reasons. For example, socket related errors may pop up or some slave cores return simple error instead of numerical results. In those scenarios, setting `sequentialRun = TRUE` may help, but it will take more time to run. Default is `FALSE`.
 ##' @param standardize This takes a logical value `TRUE` or `FALSE`. If `TRUE`, all design matrix X in phase 1 and phase 2 will be standardized in the analyses. Default is `FALSE`.
 ##' @param nRefMaxForEsti The maximum number of reference taxa used in phase 2. The default is `1`.
 ##' @param bootB Number of bootstrap samples for obtaining confidence interval of estimates in phase 2. The default is `500`.
@@ -48,7 +49,9 @@
 ##'                 fwerRate = 0.25,
 ##'                 bootB = 5)
 ##'
-##' @references \url{https://arxiv.org/pdf/1909.10101.pdf}
+##' @references Li et al.(2020) IFAA: Robust association identification and Inference For Absolute Abundance in microbiome analyses. \url{https://arxiv.org/pdf/1909.10101.pdf}
+##' @references Freedman and Lane (1983) A nonstochastic interpretation of reported significance levels. Journal of Business & Economic Statistics. 1(4):292-298.
+
 ##' @export
 ##' @md
 
@@ -72,10 +75,10 @@ IFAA=function(
   reguMethod=c("mcp"), 
   fwerRate=0.25,
   paraJobs=NULL,
+  sequentialRun=F,
   bootB=500,
   bootLassoAlpha=0.05,
   standardize=F,
-  sequentialRun=F,
   allFunc=allUserFunc(),
   refReadsThresh=0.2,
   SDThresh=0.05,
