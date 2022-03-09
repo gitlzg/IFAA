9875b9083da07a86244f6c7049c15769ed641b74
Zhigang Li <lzg2151@gmail.com>
2022-03-08 22:33:29 -0500

    v1.0.5

--- R/data.R
+++ R/data.R
@@ -2,13 +2,13 @@
 #'
 #' A dataset contains 60 taxa with absolute abundances and these are gut microbiome.
 #'
-#' @format A data frame with 20 rows and 60 variables:
+#' @format A data frame with 40 rows and 61 variables:
 "dataM"
 
 
 #' Sample covariates data
 #'
-#' A dataset ontains 5 covariates.
+#' A dataset ontains 3 covariates.
 #'
-#' @format A data frame with 20 rows and 60 variables:
+#' @format A data frame with 40 rows and 4 variables:
 "dataC"
